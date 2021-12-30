436b4f3d4432bbc22c5ce6a2b1215830e00c12ed
Rafael <rafaelproinfo@gmail.com>
2021-12-30 02:31:54 -0300

    Criar Classe PostagemController

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,28 @@
+package com.generation.blogpessoal.controller;
+
+import java.util.List;
+
+import com.generation.blogpessoal.model.Postagem;
+import com.generation.blogpessoal.repository.PostagemRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(value = "*", allowedHeaders = "*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository postagemRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+
+}
