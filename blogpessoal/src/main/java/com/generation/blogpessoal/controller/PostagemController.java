9961ddb0b80abaf7a7eaa9e0eb44cb6e33e0a8e3
Rafael <rafaelproinfo@gmail.com>
2021-12-30 13:22:53 -0300

    PostagemController - Criar Método getByTitulo

--- blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -34,4 +34,9 @@ public class PostagemController {
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+
 }
--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,11 +1,15 @@
 package com.generation.blogpessoal.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
 import com.generation.blogpessoal.model.Postagem;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+    List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 
 }
