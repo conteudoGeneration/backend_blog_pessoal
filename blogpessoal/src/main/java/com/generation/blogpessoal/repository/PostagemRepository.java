f6ceae098689257724341ac34308f5ee2cc118a4
Rafael <rafaelproinfo@gmail.com>
2021-12-30 01:28:52 -0300

    Criar Interface PostagemRepository

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,11 @@
+package com.generation.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.blogpessoal.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+}
