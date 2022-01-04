41ca97cd6ec40fb9696cf2c62cd7c1560a584732
Rafael <rafaelproinfo@gmail.com>
2022-01-04 01:30:04 -0300

    Criar Documentação com SpringDocs + Swagger

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -0,0 +1,63 @@
+package com.generation.blogpessoal.configuration;
+
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI springBlogPessoalOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+					.title("Projeto Blog Pessoal")
+					.description("Projeto Blog Pessoal - Generation Brasil")
+					.version("v0.0.1")
+				.license(new License()
+					.name("Generation Brasil")
+					.url("https://brazil.generation.org/"))
+				.contact(new Contact()
+					.name("Conteudo Generation")
+					.url("https://github.com/conteudoGeneration")
+					.email("conteudogeneration@gmail.com")))
+				.externalDocs(new ExternalDocumentation()
+					.description("Github")
+					.url("https://github.com/conteudoGeneration/"));
+	}
+
+	@Bean
+	public OpenApiCustomiser customerGlobalHeaderOpenApiCustomiser() {
+
+		return openApi -> {
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+
+				ApiResponses apiResponses = operation.getResponses();
+
+				apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+				apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+				apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
+				apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+				apiResponses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
+				apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
+				apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+
+			}));
+		};
+	}
+
+	private ApiResponse createApiResponse(String message) {
+
+		return new ApiResponse().description(message);
+
+	}
+	
+}
