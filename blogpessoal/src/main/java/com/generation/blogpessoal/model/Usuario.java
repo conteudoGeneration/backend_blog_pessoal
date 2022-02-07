7ba64effb8d237b506e024d3e6df53ca6d8ac31a
Rafael <rafaelproinfo@gmail.com>
2022-02-07 01:38:46 -0300

    Update Swagger

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
@@ -31,6 +33,7 @@ public class Usuario {
 	message = "O link da foto não pode ser maior do que 5000 caracteres")
 	private String foto;
 
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O atributo Usuário é Obrigatório!")
 	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
