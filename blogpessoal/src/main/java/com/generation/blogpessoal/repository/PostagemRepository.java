9a3315eb734608bc2394be4693962a7dee575114
Rafael <rafaelproinfo@gmail.com>
2022-06-20 20:35:32 -0300

    Update PostagemRepository

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import com.generation.blogpessoal.model.Postagem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
-    List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
