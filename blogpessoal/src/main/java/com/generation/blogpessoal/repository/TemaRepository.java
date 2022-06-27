de97801133f54365c831aa4460b7a97efa15d8c9
Rafael <rafaelproinfo@gmail.com>
2022-06-27 03:47:06 -0300

    Update Deploy

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -3,6 +3,7 @@ package com.generation.blogpessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Tema;
@@ -10,6 +11,6 @@ import com.generation.blogpessoal.model.Tema;
 @Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
 	
-	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
 
 }
